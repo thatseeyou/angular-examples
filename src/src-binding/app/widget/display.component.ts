997d293001c9efb72360d57a2dc5271153d05c95
Sang Young Oh <sangyoung@gmail.com>
2017-12-06 01:17:24 +0900

    debugging code

--- src/src-binding/app/widget/display.component.ts
+++ src/src-binding/app/widget/display.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
 
 @Component({
     selector: 'widget-display',
     templateUrl: './display.component.html',
     styleUrls: ['./display.component.css']
 })
-export class DisplayComponent implements OnInit {
+export class DisplayComponent implements OnInit, OnChanges {
     @Input() value:string = '0'
 
-    constructor() { }
+    constructor() { 
+        console.log('********* constructor')
+        console.log(`value = ${this.value}`)
+
+    }
 
     ngOnInit() { }
+
+    ngOnChanges(changes: SimpleChanges) {
+        console.log('******** ngOnChanges')
+        console.dir(changes)
+    }
 }
